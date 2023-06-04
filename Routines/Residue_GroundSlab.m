bd609503918ea68120ce2c9d50340317ec48b9eb
caginsari <73826088+caginsari@users.noreply.github.com>
2023-06-04 02:29:11 +0200

    Updated residue calculation routine.

--- Routines/Residue_GroundSlab.m
+++ Routines/Residue_GroundSlab.m
@@ -6,23 +6,18 @@ function [Vr,Ir] = Residue_GroundSlab(k0,er,h,ksw,z,f,TE_TM_flag)
 % er: is the relative permittivity of the material Air/Slab
 % air_slab_flag : the input must be Air, or Slab
 % TE_TM_flag : Either TE or TM
-ks = k0 *sqrt(er) ;
+
 deltk = k0 ./500 ;
 Dkrhop = ksw + deltk./2 ;
 Dkrhom = ksw - deltk./2 ;
 
-Dp = Den_GroundSlab(ks,er,h,Dkrhop,f,TE_TM_flag) ;
-Dm = Den_GroundSlab(ks,er,h,Dkrhom,f,TE_TM_flag) ;
+Dp = Den_GroundSlab(k0,er,h,Dkrhop,f,TE_TM_flag) ;
+Dm = Den_GroundSlab(k0,er,h,Dkrhom,f,TE_TM_flag) ;
 Dprime = (Dp-Dm) ./ deltk ;
 
-[Vr(z < h),Ir(z < h)] = Residue_GroundSlab_Slab(k0,ksw,er,h,z,TE_TM_flag,Dprime);
+[Vr,Ir] = Residue_GroundSlab_Slab(k0,ksw,er,h,z,TE_TM_flag,Dprime);
 [Vr(z > h),Ir(z > h)] = Residue_GroundSlab_Air(k0,er,h,ksw,z(z > h),TE_TM_flag,Dprime);
  
 end
 
-% if 1==strcmpi(air_slab_flag,'Air')
-%     [Vtmr,Itmr] = Residue_GroundSlab_Air(k0,er,h,ksw,z,TE_TM_flag,Dprime);
-% elseif 1==strcmpi(air_slab_flag,'Slab')
-%     [Vtmr,Itmr] = Residue_GroundSlab_Slab(k0,ksw,er,h,z,TE_TM_flag,Dprime);
-% else
-%     error('Must be one of the following inputs \n 1.Air \n 2.Slab \n Your input is %s',air_slab_flag)
+% Function checked,produces precise results
