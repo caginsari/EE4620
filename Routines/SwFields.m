1fba104a9d5dea1c99502dae040f85ad24629c46
caginsari <73826088+caginsari@users.noreply.github.com>
2023-06-10 19:45:18 +0200

    Assignment 3 finalised but there is probably still an error with the question 3 surface wave efficiency plot, specifically with the uniform current distribution. Other than that, there are minor mismatches with collegue's results .

--- Routines/SwFields.m
+++ Routines/SwFields.m
@@ -1,4 +1,4 @@
-function [Erho,Ephi,Ez] = SwFields(k0,ksw,er,Vr,Ir,rho,phi,l,w)
+function [Erho,Hphi,Ez] = SwFields(k0,ksw,er,Vr,Ir,rho,phi,l,w)
 %% EE4620 Assignment 3 : [Erho,Ephi,Ez] = SwFields(k0,ksw,er,Vr,Ir,TE_TM_flag,rho,phi)
 % Calculates the field components of the surface waves
 % Vr and Ir could be both te and tm. For field components in TE and TM
@@ -18,6 +18,6 @@ C = 1i.*sqrt(ksw./(2.*pi)).*exp(1i.*pi./4) ;
 
 Erho = Vr .* JxFT .* C .*cos(phi) .*exp(-1i.* ksw .*rho)./ sqrt(rho) ;
 Ez = -zeta.*ksw./k .* Ir .* JxFT .* C .* cos(phi) .*exp(-1i.* ksw .*rho)./ sqrt(rho) ;
-Ephi = Ir .* JxFT .* C .*cos(phi) .*exp(-1i.* ksw .*rho)./ sqrt(rho) ;
+Hphi = Ir .* JxFT .* C .*cos(phi) .*exp(-1i.* ksw .*rho)./ sqrt(rho) ;
 
 end
